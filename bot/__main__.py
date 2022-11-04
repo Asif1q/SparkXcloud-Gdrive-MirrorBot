3f7718f153a71e70f32f1b14f2e69367c9d529a1
Asif1q <54772037+Asif1q@users.noreply.github.com>
2022-11-04 23:20:22 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,5 +1,3 @@
-import os
-replNeverSleep.awake('https://'+str(os.environ['REPL_SLUG']).lower()+'.'+str(os.environ['REPL_OWNER']).lower()+'.repl.co', True)
 from signal import signal, SIGINT
 from os import path as ospath, remove as osremove, execl as osexecl
 from subprocess import run as srun, check_output
